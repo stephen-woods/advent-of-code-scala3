7a3ad3b74b9717e468a3f5ebcbb2533af29ee56e
Stephen Woods <stephen.woods@altarasystems.com>
2023-12-17 15:57:06 -0500

    Formatting 2021 1

--- src/main/scala/year2021/Day01.scala
+++ src/main/scala/year2021/Day01.scala
@@ -32,9 +32,21 @@ import scala.io.Source
   *
   * For example, suppose you had the following report:
   *
-  * 199 200 208 210 200 207 240 269 260 263 This report indicates that, scanning
-  * outward from the submarine, the sonar sweep found depths of 199, 200, 208,
-  * 210, and so on.
+  * {{{
+  * 199
+  * 200
+  * 208
+  * 210
+  * 200
+  * 207
+  * 240
+  * 269
+  * 260
+  * 263
+  * }}}
+  *
+  * This report indicates that, scanning outward from the submarine, the sonar
+  * sweep found depths of 199, 200, 208, 210, and so on.
   *
   * The first order of business is to figure out how quickly the depth
   * increases, just so you know what you're dealing with - you never know if the
@@ -45,10 +57,20 @@ import scala.io.Source
   * previous measurement. (There is no measurement before the first
   * measurement.) In the example above, the changes are as follows:
   *
-  * 199 (N/A - no previous measurement) 200 (increased) 208 (increased) 210
-  * (increased) 200 (decreased) 207 (increased) 240 (increased) 269 (increased)
-  * 260 (decreased) 263 (increased) In this example, there are 7 measurements
-  * that are larger than the previous measurement.
+  * {{{
+  * 199 (N/A - no previous measurement)
+  * 200 (increased)
+  * 208 (increased)
+  * 210 (increased)
+  * 200 (decreased)
+  * 207 (increased)
+  * 240 (increased)
+  * 269 (increased)
+  * 260 (decreased)
+  * 263 (increased)
+  * }}}
+  * In this example, there are 7 measurements that are larger than the previous
+  * measurement.
   *
   * How many measurements are larger than the previous measurement?
   *
@@ -60,12 +82,24 @@ import scala.io.Source
   * Instead, consider sums of a three-measurement sliding window. Again
   * considering the above example:
   *
-  * 199 A 200 A B 208 A B C 210 B C D 200 E C D 207 E F D 240 E F G 269 F G H
-  * 260 G H 263 H Start by comparing the first and second three-measurement
-  * windows. The measurements in the first window are marked A (199, 200, 208);
-  * their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208,
-  * 210); its sum is 618. The sum of measurements in the second window is larger
-  * than the sum of the first, so this first comparison increased.
+  * {{{
+  * 199  A
+  * 200  A B
+  * 208  A B C
+  * 210    B C D
+  * 200  E   C D
+  * 207  E F   D
+  * 240  E F G
+  * 269    F G H
+  * 260      G H
+  * 263        H
+  * }}}
+  *
+  * Start by comparing the first and second three-measurement windows. The
+  * measurements in the first window are marked A (199, 200, 208); their sum is
+  * 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its
+  * sum is 618. The sum of measurements in the second window is larger than the
+  * sum of the first, so this first comparison increased.
   *
   * Your goal now is to count the number of times the sum of measurements in
   * this sliding window increases from the previous sum. So, compare A with B,
@@ -75,10 +109,18 @@ import scala.io.Source
   * In the above example, the sum of each three-measurement window is as
   * follows:
   *
-  * A: 607 (N/A - no previous sum) B: 618 (increased) C: 618 (no change) D: 617
-  * (decreased) E: 647 (increased) F: 716 (increased) G: 769 (increased) H: 792
-  * (increased) In this example, there are 5 sums that are larger than the
-  * previous sum.
+  * {{{
+  * A: 607 (N/A - no previous sum)
+  * B: 618 (increased)
+  * C: 618 (no change)
+  * D: 617 (decreased)
+  * E: 647 (increased)
+  * F: 716 (increased)
+  * G: 769 (increased)
+  * H: 792 (increased)
+  * }}}
+  *
+  * In this example, there are 5 sums that are larger than the previous sum.
   *
   * Consider sums of a three-measurement sliding window. How many sums are
   * larger than the previous sum?
