8703af90f5f6232ac54f55af4f67d710f746e88d
Stephen Woods <stephen.woods@altarasystems.com>
2023-12-23 01:39:25 -0500

    Start of 2023 5

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -54,6 +54,7 @@ object Main extends ZIOAppDefault {
              case (2023, 2) => year2023.Day02.run()
              case (2023, 3) => year2023.Day03.run()
              case (2023, 4) => year2023.Day04.run()
+             case (2023, 5) => year2023.Day05.run()
              case _         =>
                Console.printLineError(s"--- Day $day $year: Not Implemented!")
            }
